b920287c7ac83d0dfef231044f75ceab21209013
Laurent Aphecetche <laurent.aphecetche@gmail.com>
2020-12-21 19:35:25 +0100

    hugo: change github url to fetch tarball

--- packages/hugo/package.py
+++ packages/hugo/package.py
@@ -12,16 +12,15 @@ class Hugo(Package):
     """The world's fastest framework for building websites."""
 
     homepage = "https://gohugo.io"
-    url = "https://github.com/gohugoio/hugo/archive/v0.53.tar.gz"
+    url = "https://api.github.com/repos/gohugoio/hugo/tarball/v0.79.1"
 
     executables = ['^hugo$']
 
-    version(
-        '0.35', sha256='dfef416fe3a0355caef0210d0e7530383f6ec320ce0891431ab7c039b31a86c4')
+    version('0.79.1', sha256='d0192e3c4e6c10c150b0b6aafb89c80c1434a206f3bb9ceeb88e75df5236e75d')
     version('0.74.3', sha256='9b296fa0396c20956fa6a1f7afadaa78739af62c277b6c0cfae79a91b0fe823f')
     version('0.68.3', sha256='38e743605e45e3aafd9563feb9e78477e72d79535ce83b56b243ff991d3a2b6e')
-    version(
-        '0.53', sha256='48e65a33d3b10527101d13c354538379d9df698e5c38f60f4660386f4232e65c')
+    version('0.53', sha256='48e65a33d3b10527101d13c354538379d9df698e5c38f60f4660386f4232e65c')
+    version('0.35', sha256='dfef416fe3a0355caef0210d0e7530383f6ec320ce0891431ab7c039b31a86c4')
 
     # Uses go modules.
     # See https://gohugo.io/getting-started/installing/#fetch-from-github
