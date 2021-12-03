27e663057366c8b69810d70cf3414a4cf051ed5d
Laurent Aphecetche <laurent.aphecetche@cern.ch>
2021-12-03 19:08:24 +0100

    m2-exp-alice with aliroot-lite and aliphysics-lite

--- packages/m2-exp-alice/package.py
+++ packages/m2-exp-alice/package.py
@@ -10,21 +10,19 @@ from spack import *
 class M2ExpAlice(CMakePackage):
     """FIXME: Put a proper description of your package here."""
 
-    # FIXME: Add a proper url for your package's homepage here.
     homepage = "https://www.example.com"
     git      = "https://github.com/aphecetche/m2-exp-alice.git"
     url      = "https://github.com/aphecetche/m2-exp-alice/archive/refs/tags/v0.0.1.tar.gz"
     
     extends('python')
     generator = "Ninja"
-    # FIXME: Add a list of GitHub accounts to
-    # notify when the package is updated.
-    # maintainers = ['github_user1', 'github_user2']
 
     version('dev',branch='dev')
     version('0.0.1', sha256='0e9fb4f778417a62170888d558426107f30c64d64634e3799e38f37278c5998d')
 
-    # FIXME: Add dependencies if required.
+    variant('aliroot',default=True,description='use aliroot-lite library')
+    variant('aliphysics',default=True,description='use aliphysics-lite library',when='+aliroot')
+
     depends_on('root')
     depends_on('fmt')
     depends_on('py-pybind11',type='build')
@@ -32,10 +30,11 @@ class M2ExpAlice(CMakePackage):
     depends_on('boost')
     depends_on('rapidjson')
     depends_on('ninja',type='build')
+    depends_on('jalien-root',when='+aliroot')
 
     def cmake_args(self):
-        # FIXME: Add arguments other than
-        # FIXME: CMAKE_INSTALL_PREFIX and CMAKE_BUILD_TYPE
-        # FIXME: If not needed delete this function
         args = []
+        args.append(self.define_from_variant("BUILD_ALIROOT","aliroot"))
+        args.append(self.define_from_variant("BUILD_ALIPHYSICS","aliphysics"))
+        args.append(self.define("CMAKE_EXPORT_COMPILE_COMMANDS",True))
         return args
