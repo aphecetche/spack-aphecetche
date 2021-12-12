7ad362fb87b485ecbd9ee0064df93c6a33b592dd
Laurent Aphecetche <laurent.aphecetche@cern.ch>
2021-12-12 13:15:50 +0100

    more variants to m2-exp-alice

--- packages/m2-exp-alice/package.py
+++ packages/m2-exp-alice/package.py
@@ -22,25 +22,44 @@ class M2ExpAlice(CMakePackage):
 
     variant('aliroot',default=True,description='use aliroot-lite library')
     variant('aliphysics',default=True,description='use aliphysics-lite library',when='+aliroot')
+    variant('converters',default=True,description='build various data converters')
+    variant('tracking',default=True,description='build tracking library')
+    variant('mapping',default=True,description='build mapping library')
 
     depends_on('root')
     depends_on('fmt')
     depends_on('py-pybind11',type='build')
+    depends_on('py-pytest',type='build')
     depends_on('py-vector')
     depends_on('boost')
     depends_on('rapidjson')
     depends_on('ninja',type='build')
     depends_on('jalien-root',when='+aliroot')
     depends_on('vmc',when='+aliphysics')
+    depends_on('fairlogger',when='+tracking')
+    depends_on('cppgsl',when='+mapping')
+    depends_on('py-jupyterlab')
+    depends_on('py-pandas')
+    depends_on('py-matplotlib')
+    depends_on('py-uproot')
+    depends_on('py-awkward')
 
     def cmake_args(self):
         args = []
-        args.append(self.define_from_variant("BUILD_ALIROOT","aliroot"))
         args.append(self.define_from_variant("BUILD_ALIPHYSICS","aliphysics"))
+        args.append(self.define_from_variant("BUILD_ALIROOT","aliroot"))
+        args.append(self.define_from_variant("BUILD_CONVERTERS","converters"))
+        args.append(self.define_from_variant("BUILD_MAPPING","mapping"))
+        args.append(self.define_from_variant("BUILD_TRACKING","tracking"))
         args.append(self.define("CMAKE_EXPORT_COMPILE_COMMANDS",True))
         return args
 
+    def setup_build_environment(self,env):
+        env.set('O2_ROOT',os.path.join(self.build_directory,'stage'))
+        env.prepend_path('PYTHONPATH',os.path.join(self.build_directory,'stage/lib'))
+
     def setup_run_environment(self,env):
+        env.set('O2_ROOT',self.prefix)
         env.set('ALICE_ROOT',self.prefix)
         env.set('ALICE_PHYSICS',self.prefix)
         env.append_path('ROOT_INCLUDE_PATH',os.path.join(self.prefix,'include'))
